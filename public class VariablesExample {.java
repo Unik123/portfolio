86286d072738312e16ab55b55fbce89275bd6fef
Unik123 <unikguru123@gmail.com>
2024-07-18 15:35:21 +0545

    Portfolio(Need to be developed)

---
+++ public class VariablesExample {.java
@@ -0,0 +1,11 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int num1 = 10;
+        double num2 =5.5;
+        char letter ='A';
+
+        int sum = num1 + (int) num2;
+        System.out.println("Sum: " + sum);
+        System.out.println("Letter: " + letter);
+    }
+}
